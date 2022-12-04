d34188690ab6de85faa084f1071f87bf4443830a
MylesFord <118659263+MylesFord@users.noreply.github.com>
2022-12-04 15:56:13 -0500

    Update threading.py

--- threading.py
+++ threading.py
@@ -1,16 +1,6 @@
-import threading
-
-start = time.time()
-
-square_thread = threading.Thread(target=calc_square, args=(numbers,))
-cube_thread = threading.Thread(target=calc_cube, args=(numbers,))
-
-square_thread.start()
-cube_thread.start()
-
-square_thread.join()
-cube_thread.join()
-
-end = time.time()
-
-print('Execution Time: {}'.format(end-start))
+from threading import *
+def show()
+  print("Child Thread")
+t = Thread(target=show())
+t.start()
+print("parent thread")
