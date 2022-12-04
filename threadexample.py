b4c1eed5e45305396ed3c0e4f5304f48a6548cb6
MylesFord <118659263+MylesFord@users.noreply.github.com>
2022-12-04 16:31:56 -0500

    Update threadexample.py

--- threadexample.py
+++ threadexample.py
@@ -10,7 +10,8 @@ def print_2():
   print('starting of thread :', threading.currentThread().name)
   print('finishing of thread :', threading.currentThread().name)
   
-a = threading.Thread(target=print_1, name='thread-1', daemon=True)
+a = threading.Thread(target=print_1, name='thread-1')
+daemon = True
 b = threading.Thread(target=print_2, name='thread-2')
 
 a.start()
