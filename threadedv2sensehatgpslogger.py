63c8202f49c1dc199bf13ab2b59c9d13e2dcc87a
MylesFord <118659263+MylesFord@users.noreply.github.com>
2022-12-06 00:14:02 -0500

    Update threadedv2sensehatgpslogger.py

--- threadedv2sensehatgpslogger.py
+++ threadedv2sensehatgpslogger.py
@@ -50,7 +50,7 @@ def file_setup1(filename):
 def file_setup2(filename2):
     header2 =[]
 
-    header2.append("sensehat_Logger\r")
+    header2.append("sensehat_Logger\r\n")
 
     if TEMP_H:
         header2.append("temp_h")
