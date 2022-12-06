b34065a0726262d14d88170c76583d635de8eab1
MylesFord <118659263+MylesFord@users.noreply.github.com>
2022-12-06 02:07:49 -0500

    Update threadedv2sensehatgpslogger.py

--- threadedv2sensehatgpslogger.py
+++ threadedv2sensehatgpslogger.py
@@ -100,14 +100,14 @@ def get_gps_data():
 		return sense_data
 
 def get_hat_data():
-#    localtime = datetime.now()
- #   ##current_time = str(localtime.hour)+":"+str(localtime.minute)+":"+str(localtime.second)+"."+str(localtime.microsecond)
-  #  current_time = str(datetime.now().time()) ## for simplified mega log viewer 
-   # ##current_time = time.time() ## for mclaren atlas
-    #log_string = current_time[:-3] ##strip last three time decimals to keep atlas happy
+    localtime = datetime.now()
+    ##current_time = str(localtime.hour)+":"+str(localtime.minute)+":"+str(localtime.second)+"."+str(localtime.microsecond)
+    current_time = str(datetime.now().time()) ## for simplified mega log viewer 
+    ##current_time = time.time() ## for mclaren atlas
+    log_string = current_time[:-3] ##strip last three time decimals to keep atlas happy
 
     
- #   sense_data2.append(log_string)  ##moved timestamp to beginning for megalogviewer compatability
+    sense_data2.append(log_string)  ##moved timestamp to beginning for megalogviewer compatability
 
     if TEMP_H:
         sense_data2.append(sense.get_temperature_from_humidity())
@@ -292,8 +292,8 @@ gpsd = gps(mode=WATCH_ENABLE) #starting the stream of info
 
 
 sense = SenseHat()
-sense_data=[]
-sense_data2=[]
+#sense_data=[]
+#sense_data2=[]
 batch_data= []
 batch_data2= []
 
