6c9593da95844ef5d8779d9b90754f6af5d61f5e
MylesFord <118659263+MylesFord@users.noreply.github.com>
2022-12-06 00:23:54 -0500

    Update threadedv2sensehatgpslogger.py

--- threadedv2sensehatgpslogger.py
+++ threadedv2sensehatgpslogger.py
@@ -70,7 +70,7 @@ def file_setup2(filename2):
         header2.extend(["gyro_x","gyro_y","gyro_z"])
 
     with open(filename2,"w") as g:
-        g.write(value in header2,"\n")
+        g.write(header2,"\n")
 
 
 
